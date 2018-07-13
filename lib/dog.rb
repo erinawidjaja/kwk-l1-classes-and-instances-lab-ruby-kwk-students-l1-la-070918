76dff404cc743c13375224437ce768bb321958a9
erinawidjaja <erina.widjaja@gmail.com>
2018-07-13 18:42:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+class Dog 
+  attr_accessor :fido, :snoopy, :lassie 
+  def initialize(fido, snoopy, lassie)
+    @fido = fido 
+      @snoopy = snoopy
+      @lassie = lassie 
+    end 
+  end 
+  dog_one = Dog.new("Fido", "Snoopy", "Lassie")
+  puts dog_one.fido
+  puts dog_one.snoopy
+  puts dog_one.lassie
+  
